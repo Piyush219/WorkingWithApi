15efc1457e2166473df983548c818344909be4ce
Piyush219 <piyush66642k@gmail.com>
2021-12-23 17:29:21 +0530

    With Async Await

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const [movies, setMovies] = useState([])
-  function fetchMovieHandler() {
-    fetch('https://swapi.dev/api/films/').then(response =>{
-      return response.json();
-    }).then(data=>{
-      const transformedMovies = data.results.map((moviesData) => {
-        return {
-          id: moviesData.episode_id,
-          title: moviesData.title,
-          openingText: moviesData.opening_crawl,
-          releaseDate: moviesData.release_date
-        }
-      })
-      setMovies(transformedMovies);
-    });
-  }
+  const [movies, setMovies] = useState([]);
+  async function fetchMovieHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+        const data = await response.json();
+        const transformedMovies = data.results.map((moviesData) => {
+          return {
+            id: moviesData.episode_id,
+            title: moviesData.title,
+            openingText: moviesData.opening_crawl,
+            releaseDate: moviesData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      }
+
 
   return (
     <React.Fragment>
